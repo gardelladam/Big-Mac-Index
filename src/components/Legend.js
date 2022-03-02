8c2da0afe45063433563c37ff07a38249141b8ff
Adam Gardell <adaga376@student.liu.se>
2022-03-02 15:45:34 +0100

    kartans färger och legend

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,23 +1,54 @@
 import L from "leaflet";
 import { useEffect } from "react";
-//import './Legend.css'
+//import getColor from "./MyMap.js"
+import './Legend.css'
+
+
+
+function getColor(d) {
+    return d > 1    ? '#053061' :
+        d > 0.7    ? '#2166ac' :
+        d > 0.5    ? '#4393c3' :
+        d > 0.3    ? '#92c5de' :
+        d > 0.1    ? '#d1e5f0' :
+        d > 0.01    ? '#f7f7f7' :
+        d > -0.01   ? '#f7f7f7' :
+        d > -0.1   ? '#f7f7f7' :     
+        d > -0.3   ? '#fddbc7' :
+        d > -0.5   ? '#f4a582' :
+        d > -0.7   ? '#d6604d' :
+        d > -1   ? '#b2182b' :
+                     '#67001f';
+                    
+}
+
+
 function Legend({ map }) {
-  console.log(map);
+
+ 
   useEffect(() => {
     if (map) {
-      const legend = L.control({ position: "bottomright" });
+      var legend = L.control({ position: "bottomright" });
+      
 
       legend.onAdd = () => {
-        const div = L.DomUtil.create("div", "info legend");
-        div.innerHTML =
-          "<h4>This is the legend</h4>" +
-          "<b>Lorem ipsum dolor sit amet consectetur adipiscing</b>";
+        var div = L.DomUtil.create("div", "info legend"),
+        grades = [-10,-1,-0.7, -0.5, -0.3, -0.1,-0.01, 0.01, 0.1, 0.3, 0.5, 0.7,1],
+        
+        labels = [];
+        for (var i = 0; i < grades.length; i++) {
+        var newElement = document.createElement('div');
+        newElement.innerHTML =
+            '<i style="background:' + getColor(grades[i]+0.001) + '"></i> ' + (grades[i] == -10 ? 'less than ' + grades[i+1] + '<br>'  :
+             + grades[i] + (grades[i + 1] ? ' ' + 'to'  + ' ' + grades[i + 1]  + '<br>' : ' and over'));
+             div.appendChild(newElement);
+      }
         return div;
       };
 
       legend.addTo(map);
     }
-  }, [map]); //here add map
+  }, [map]); 
   return null;
 }
 
